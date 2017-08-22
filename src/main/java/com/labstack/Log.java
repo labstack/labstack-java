c31dfd253ce677cc3904f793dea370c651b00d77
Vishal Rana <vr@labstack.com>
2017-08-22 12:57:53 -0700

    Bumped v0.6.32
    Signed-off-by: Vishal Rana <vr@labstack.com>

--- src/main/java/com/labstack/Log.java
+++ src/main/java/com/labstack/Log.java
@@ -32,7 +32,7 @@ public class Log implements Thread.UncaughtExceptionHandler {
         Thread.setDefaultUncaughtExceptionHandler(this);
     }
 
-    protected static String getStackTrace(Throwable throwable) {
+    public static String getStackTrace(Throwable throwable) {
         StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         throwable.printStackTrace(printWriter);
